a6a2cd70463623dd2a0e4aa834359d4ce512fa8a
Aadi9026 <97016635+Aadi9026@users.noreply.github.com>
2025-09-26 09:30:45 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,14 @@
+export const GENRES = [
+  'Action', 'Adventure', 'Animation', 'Comedy', 'Crime',
+  'Documentary', 'Drama', 'Fantasy', 'Horror', 'Mystery',
+  'Romance', 'Sci-Fi', 'Thriller', 'Western'
+];
 
+export const QUALITIES = ['480p', '720p', '1080p', '4K'];
+
+export const AD_POSITIONS = {
+  HEADER: 'header',
+  SIDEBAR: 'sidebar',
+  FOOTER: 'footer',
+  IN_VIDEO: 'in_video'
+};
