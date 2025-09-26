d65c21a925eee623e88479e4981af8ef1fca21e9
Aadi9026 <97016635+Aadi9026@users.noreply.github.com>
2025-09-26 11:24:51 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-    sourcemap: false
+    sourcemap: false,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html')
+      }
+    }
   },
   server: {
     port: 3000,
     open: true
-  }
+  },
+  publicDir: 'public'
 })
