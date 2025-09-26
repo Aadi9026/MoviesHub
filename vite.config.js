cf0ce21a4dfe5162529a6060c43bea33164eb5a8
Aadi9026 <97016635+Aadi9026@users.noreply.github.com>
2025-09-26 13:03:21 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
     sourcemap: false,
-    rollupOptions: {
-      input: {
-        main: './index.html'
-      }
-    }
+    minify: 'esbuild'
   },
   server: {
     port: 3000,
-    open: true
+    host: true
   },
-  resolve: {
-    alias: {
-      '@': '/src'
-    }
+  esbuild: {
+    jsxFactory: 'React.createElement',
+    jsxFragment: 'React.Fragment'
   }
 })
