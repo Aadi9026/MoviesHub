a798aa2732ededd7adef02c089611b0b602196fd
Aadi9026 <97016635+Aadi9026@users.noreply.github.com>
2025-10-04 13:10:01 +0530

    Update VideoCard.jsx

--- src/components/Common/VideoCard.jsx
+++ src/components/Common/VideoCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { formatDuration, formatViews } from '../../utils/helpers';
+import { formatDuration, formatViews, formatDate } from '../../utils/helpers'; // Add formatDate import
 
 const VideoCard = ({ video }) => {
   const { id, title, thumbnail, duration, views, genre, createdAt } = video;
@@ -8,22 +8,21 @@ const VideoCard = ({ video }) => {
   return (
     <div className="video-card">
       <Link to={`/video/${id}`}>
-        <div className="thumbnail-container"> {/* Changed from thumbnail to thumbnail-container */}
-          <img src={thumbnail} alt={title} className="thumbnail" /> {/* Added thumbnail class */}
+        <div className="thumbnail-container">
+          <img src={thumbnail} alt={title} className="thumbnail" />
           <div className="video-duration">{formatDuration(duration)}</div>
-          {/* Remove play-icon if not styled in CSS */}
         </div>
       </Link>
       
-      <div className="video-info-container"> {/* Changed from video-info to video-info-container */}
-        <div className="video-details"> {/* Added this wrapper */}
+      <div className="video-info-container">
+        <div className="video-details">
           <Link to={`/video/${id}`}>
             <h3 className="video-title">{title}</h3>
           </Link>
           <div className="video-meta">
             <span>{formatViews(views)} views</span>
             <span className="separator"> • </span>
-            <span>{new Date(createdAt?.toDate()).toLocaleDateString()}</span>
+            <span>{formatDate(createdAt)}</span> {/* Changed this line */}
             <span className="genre-tag">{genre}</span>
           </div>
         </div>
